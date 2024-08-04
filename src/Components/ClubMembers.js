faf06ee8d5c7a96064f7b04986e6a834859c34c8
kalekimmel <KimmelK11@gmail.com>
2024-08-04 00:35:22 -0500

    new styling

--- src/Components/ClubMembers.js
+++ src/Components/ClubMembers.js
@@ -5,7 +5,7 @@ import './ClubMembers.css';
 const ClubMembers = () => {
   const members = [
     { name: 'Paul Selborg', role: 'President', image: "../../../Images/IMG_0644.PNG" },
-    { name: 'Kale Kimmel', role: 'Vice President/Website Designer', image: "../../../Images/IMG_0098 (2).PNG" },
+    { name: 'Kale Kimmel', role: 'VP/Website Designer', image: "../../../Images/IMG_0098 (2).PNG" },
     { name: 'Rudy Wicker', role: 'Fundraiser Leader', image: '../../../Images/IMG_0630.PNG' },
     { name: 'Pranav Dronavalli', role: 'Treasurer', image:  "../../../Images/IMG_0631.PNG" },
     { name: 'Michael Crivello', role: 'Secretary', image: "../../../Images/IMG_0633.PNG"  },
